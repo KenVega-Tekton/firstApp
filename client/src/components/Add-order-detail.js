9539e46b799c033a964ee274a6225d253d6e40dd
Ken <ken.vega@tektonlabs.com>
2018-01-19 10:26:56 -0500

    i'm going to start to work(updating) on the api endpoint using good practices

--- client/src/components/Add-order-detail.js
+++ client/src/components/Add-order-detail.js
@@ -18,7 +18,58 @@ class AddOrderDetail extends Component {
   }
 
   render() {
-    return <div>Hola</div>;
+    let items = [];
+
+    return (
+      <div>
+        <div className="form-group row mt-3">
+          <label htmlFor="numberDishes" className="col-8 col-form-label">
+            Number of dishes:
+          </label>
+          <div className="col-4">
+            <input
+              type="number"
+              min="1"
+              className="form-control"
+              value={this.state.numberDishes}
+              onChange={this.onNumberDishesChange}
+              required
+            />
+          </div>
+        </div>
+
+        <hr />
+
+        {items}
+
+        <datalist id="dishesAvailable">
+          {this.state.dishes
+            ? this.state.dishes.map(dish => (
+                <option value={dish.dishName} key={dish._id} />
+              ))
+            : null}
+        </datalist>
+
+        <hr />
+
+        <div className="form-group row">
+          <label htmlFor="totalOwed" className="col-8 col-form-label">
+            Total Owed:
+          </label>
+          <div className="col-4">
+            <input
+              type="number"
+              className="form-control"
+              id="totalOwed"
+              name="totalOwed"
+              defaultValue="0"
+              readOnly
+              required
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
